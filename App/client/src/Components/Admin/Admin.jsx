41a7a2ac2abb52a19d9827eff3a10b88e6ddad87
Nikolaos Sokos <nsokos4@gmail.com>
2022-09-15 15:39:37 +0300

    Update Admin.jsx

--- App/client/src/Components/Admin/Admin.jsx
+++ App/client/src/Components/Admin/Admin.jsx
@@ -9,7 +9,7 @@ export class Admin extends Component {
  *  A) SELECT count(*) FROM visits
  *  B) SELECT count(*) FROM cases
  *  C) SELECT count(*) FROM visits INNER JOIN cases ON cases.USER_ID = visits.USER_ID WHERE visits.TIMESTAMP BETWEEN DATE_SUB(DATE_RECORDED,INTERVAL 7 DAY) AND DATE_ADD(DATE_RECORDED, INTERVAL 14 DAY)
- *  D) SELECT types->"$" as types FROM locations INNER JOIN visits ON locations.ID = visits.lOCATION_ID ORDER BY visits.count(*)
+ *  D) SELECT types , count(ID) FROM locations INNER JOIN visits ON locations.ID = visits.LOCATION_ID GROUP by ID ORDER BY count(visits.LOCATION_ID) DESC
  * SELECT comp , COUNT(*)
 FROM
 (
