1e3ecd466b31b018db840365a8c9cb9c1ee53871
Nikolaos Sokos <nsokos4@gmail.com>
2022-09-18 07:39:17 +0300

    Update ChartsComponent.jsx

--- App/client/src/Components/Admin/ChartsComponent.jsx
+++ App/client/src/Components/Admin/ChartsComponent.jsx
@@ -1,16 +1,47 @@
 import React from "react";
 import styled from "styled-components";
 //import { useState, useEffect } from "react";
-
+import { Bar } from "react-chartjs-2";
+// eslint-disable-next-line
+import Chart from "chart.js/auto";
 export const ChartsComponent = ({
   visitByCasesCountPerDay,
   visitCountPerDay,
+  data = {
+    labels: ["January", "February", "March", "April", "May"],
+    datasets: [
+      {
+        label: "Visits",
+        backgroundColor: "rgba(75,192,192,1)",
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 2,
+        data: visitCountPerDay,
+      },
+    ],
+  },
 }) => {
-  return <StatsWrapper></StatsWrapper>;
+  return (
+    <StatsWrapper>
+      <Bar
+        data={data}
+        options={{
+          title: {
+            display: true,
+            text: "User Visits",
+            fontSize: 20,
+          },
+          legend: {
+            display: true,
+            position: "right",
+          },
+        }}
+      />
+    </StatsWrapper>
+  );
 };
 
 export default ChartsComponent;
-
+// eslint-disable-next-line
 const StatsWrapper = styled.div`
   width: 50vw;
   height: 40vh;
